a6f588fab5b1046da5685f2a27804bebbf455e1c
Jai Gawande <gawande20jai@gmail.com>
2025-03-22 22:57:23 +0530

    db

--- config/db.js
+++ config/db.js
@@ -10,16 +10,7 @@ const AppDataSource = new DataSource({
     entities: [require('../models/Product')],
     synchronize: true, // Auto-create tables
 });
-// const AppDataSource = new DataSource({
-//     type: 'mysql',
-//     host: 'localhost',
-//     port: 3306,
-//     username: 'root',
-//     password: 'krsna$02',
-//     database: 'ecommerce_db',
-//     entities: [require('../models/Product')],
-//     synchronize: true, // Auto-create tables
-// });
+
 
 AppDataSource.initialize()
     .then(() => console.log('Database connected'))
